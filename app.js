d6760e6a3d279f3733b4a26db42753bd42ea572b
Gbissell93 <gregorybisselledu@gmail.com>
2021-10-13 12:51:36 -0400

    env set, first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+//dependencies
+require("dotenv").config();
+var createError = require("http-errors");
+var express = require("express");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+//establish env connection mongodb
+mongoose
+  .connect(process.env.MONGO_DB)
+  .then(() => {
+    console.log("MONGODB CONNECTED");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+//routes section
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.json({ message: "error", error: error.message });
+});
+
+module.exports = app;
